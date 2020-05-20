16118389ee6c32533be1c478f0c5ca98a1ded87a
Jnmendza <jonatmendoza@gmail.com>
2020-05-20 16:24:13 -0700

    added dataset to .barca-card and working match()

--- scripts.js
+++ scripts.js
@@ -14,20 +14,22 @@ function flipCard() {
         // second click
         hasFlipped = false;
         secondCard = this;   
+
+        checkMatch()
     }
 }
 
 function checkMatch() {
     // match the cards
-        if(firstCard.dataset.framework === secondCard.dataset.framework) {
-            firstCard.removeEventListener("click", flipCard);
-            secondCard.removeEventListener("click", flipCard);
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove('flip')
-                secondCard.classList.remove('flip')
-            }, 1500)
-        }
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove('flip')
+            secondCard.classList.remove('flip')
+        }, 1500)
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
